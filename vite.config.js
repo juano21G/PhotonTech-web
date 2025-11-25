dfb21bb254fe34308947e81c325379fd4d14a8e2
hisxkx <est.juanj.gutierrez@unimilitar.edu.co>
2025-11-25 10:37:13 -0500

    Fix: Base path conditional for local/prod

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-    tailwindcss(),
-  ],
 
-  base: "/PhotonTech-web/",
+// Usamos una función para acceder al estado de la ejecución
+export default defineConfig(({ command }) => {
   
-})
+  // Si el comando es 'build' (npm run deploy), usamos la ruta de GitHub.
+  // Si el comando es 'serve' (npm run dev), usamos la raíz (/).
+  const isProd = command === 'build';
+  const base = isProd ? '/PhotonTech-web/' : '/';
+
+  console.log(`Vite running in mode: ${command}. Base path set to: ${base}`);
+
+  return {
+    base,
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+  }
+});
